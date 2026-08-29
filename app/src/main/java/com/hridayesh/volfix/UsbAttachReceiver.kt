d4caf95d68e4a186e58ca6f23b361de92307120f
Hridayesh J <hridayeshjagadesan@gmail.com>
2026-08-29 18:38:51 +0530

    add missing imports

--- app/src/main/java/com/hridayesh/volfix/UsbAttachReceiver.kt
+++ app/src/main/java/com/hridayesh/volfix/UsbAttachReceiver.kt
@@ -1,3 +1,11 @@
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.hardware.usb.UsbDevice
+import android.hardware.usb.UsbDeviceConnection
+import android.hardware.usb.UsbManager
+
 class UsbAttachReceiver : BroadcastReceiver() {
     override fun onReceive(ctx: Context, intent: Intent) {
         val device = intent.getParcelableExtra<UsbDevice>(UsbManager.EXTRA_DEVICE) ?: return
